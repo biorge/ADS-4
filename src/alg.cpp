e28bf78039b682e67ebc6d314e963c2d4cfd5ab2
Anton <150729033+biorge@users.noreply.github.com>
2024-03-23 14:05:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,63 +1,68 @@
 // Copyright 2021 NNTU-CS
-int countPairsInRange(int *arr, int left, int right, int value) {
+int countPairs1(int* arr, int len, int value) {
   int count = 0;
-  while (left < right) {
-    int sum = arr[left] + arr[right];
-    if (sum == value) {
-      count++;
-      left++;
-      right--;
-    } else if (sum < value) {
-      left++;
-    } else {
-      right--;
+  for (int i = 0; i < len; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if (arr[i] + arr[j] == value) {
+        count++;
+      }
     }
   }
-  return count;
+  if (count == 0)
+    return 0;
+  else
+    return count;
 }
 
-int countPairs1(int *arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len; ++i) {
-    count += countPairsInRange(arr, i + 1, len - 1, value - arr[i]);
-  }
-  return count;
-}
-
-int countPairs2(int *arr, int len, int value) {
+int countPairs2(int* arr, int len, int value) {
   int count = 0;
   int right = len - 1;
   while (arr[right] > value) {
     right--;
   }
   for (int left = 0; left < right; left++) {
-    count += countPairsInRange(arr, left + 1, right, value - arr[left]);
-  }
-  return count;
-}
-
-int binarySearch(int *arr, int left, int right, int target) {
-  while (left <= right) {
-    int mid = left + (right - left) / 2;
-    if (arr[mid] == target) {
-      return mid;
-    } else if (arr[mid] < target) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
+    for (int r = right; left < r; r--) {
+      if (arr[left] + arr[r] == value) {
+        count++;
+      }
     }
   }
-  return -1;
+  if (count == 0)
+    return 0;
+  else
+    return count;
 }
 
-int countPairs3(int *arr, int len, int value) {
+int countPairs3(int* arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; ++i) {
-    int sumElem = value - arr[i];
-    int index = binarySearch(arr, i + 1, len - 1, sumElem);
-    if (index != -1 && arr[index] == sumElem) {
-      count++;
+    int left = i + 1;
+    int right = len - 1;
+    int sumChar = value - arr[i];
+    while (left <= right) {
+      int midInd = (left + right) / 2;
+      if (arr[midInd] == sumChar) {
+        count++;
+        int mid = midInd - 1;
+        while ((mid > i) && (arr[mid] == arr[midInd])) {
+          count++;
+          mid--;
+        }
+        int na = midInd + 1;
+        while ((i < na) && (arr[na] == arr[midInd])) {
+          count++;
+          na++;
+        }
+        break;
+      } else if (arr[midInd] > sumChar) {
+        right = midInd - 1;
+      } else {
+        left = midInd + 1;
+      }
     }
   }
-  return count;
+  if (count)
+    return count;
+  else
+    return 0;
 }
